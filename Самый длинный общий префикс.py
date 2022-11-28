1e081739edcdfecf6e33b46979d98634c6dc877e
Elizabeth <92400879+ElizabethGlebezdova@users.noreply.github.com>
2022-11-28 19:01:00 +0300

    Create Самый длинный общий префикс.py

---
+++ Самый длинный общий префикс.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def f(a):
+    size = len(a)
+    if (size == 0):
+        return ""
+    if (size == 1):
+        return a[0]
+    a.sort()
+    end = min(len(a[0]), len(a[size - 1]))
+    i = 0
+    while (i < end and a[0][i] == a[size - 1][i]):
+        i += 1
+    pre = a[0][0: i]
+    return pre
+
+k = int(input())
+s = []
+for i in range(k):
+    s.append(input())
+print(f(s))

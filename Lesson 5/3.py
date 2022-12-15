07d550862edd1ec76b556dd427cfc06a6e396552
Elizabeth <92400879+ElizabethGlebezdova@users.noreply.github.com>
2022-12-15 12:01:10 +0300

    Update 3.py

--- Lesson 5/3.py
+++ Lesson 5/3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def fibonacci(n):
+    if (n == 0):
+        return 0
     if (n == 1) or (n == 2):
         return 1
     return fibonacci(n - 1) + fibonacci(n - 2)
@@ -8,7 +10,10 @@ def fibonacci(n):
 while(True):
     try:
         n = int(input('Введите номер числа Фибоначчи: '))
+        fib = fibonacci(n)
         break
     except ValueError:
         print('Неверный ввод, попробуйте еще раз!')
-print('Заданное число Фибоначчи равно', fibonacci(n))
+    except RecursionError:
+        print('Число не может быть отрицательным, попробуйте еще раз!')
+print('Заданное число Фибоначчи равно', fib)

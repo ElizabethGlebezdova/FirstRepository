d97044a0ec0d4e7c622794c374295a3b5e11e58d
Elizabeth <92400879+ElizabethGlebezdova@users.noreply.github.com>
2022-12-23 01:30:42 +0300

    Update 1.py

--- Lesson 6/1.py
+++ Lesson 6/1.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 
-def stringsToBytecodes(list):
+def strings_to_bytecodes(list):
     """Перевод списка строк в список байт кодов"""
     byteCodes = []
     for i in list:
         byteCodes.append(i.encode('utf-8'))
     return byteCodes
 
-def bytecodesToStrings(list):
+def bytecodes_to_strings(list):
     """Перевод списка байт кодов в список строк"""
     strings = []
     for i in list:
         strings.append(i.decode('utf-8'))
     return strings
 
-listOfStr = []
+list_of_str = []
 str = input('Введите строки(конец - 0):\n')
 while(str != '0'):
-    listOfStr.append(str)
+    list_of_str.append(str)
     str = input()
-listOfBytes = stringsToBytecodes(listOfStr)
-print(f'Список строк в список байт кодов:\n{listOfBytes}')
-print(f'Список байт кодов в список строк:\n{bytecodesToStrings(listOfBytes)}')
+list_of_bytes = strings_to_bytecodes(list_of_str)
+print(f'Список строк в список байт кодов:\n{list_of_bytes}')
+print(f'Список байт кодов в список строк:\n{bytecodes_to_strings(list_of_bytes)}')

8617dd9e6ea6e0ce43e74dfdbd11f3ed5fe19ecc
Elizabeth <92400879+ElizabethGlebezdova@users.noreply.github.com>
2022-12-22 15:45:35 +0300

    Update 4.py

--- Lesson 7/4.py
+++ Lesson 7/4.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
+
 import os
 
 print(f'Имя операционной системы: {os.name}')
-print(f'Имя пользователя, вошедшего в терминал: {os.getlogin}')
-print(f'Список файлов и директорий в папке:\n{os.listdir}')
+print(f'Имя пользователя, вошедшего в терминал: {os.getlogin()}')
+print(f'Список файлов и директорий в папке:\n{os.listdir(r"C:/")}')
